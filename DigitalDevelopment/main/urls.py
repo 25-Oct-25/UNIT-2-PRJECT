85d6ef20959dcb80b90c797a74f2489279137383
Hissah Saad <hissahsaad23@gmail.com>
2025-11-06 12:27:00 +0300

    create website pages - phase2

--- DigitalDevelopment/main/urls.py
+++ DigitalDevelopment/main/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("", views.home_view, name="home_view")
+    path("", views.home_view, name="home_view"),
+    path("fields/", views.fields_view, name="fields_view"),
+    path("future/", views.future_view, name="future_view"),
+    path("impact/", views.impact_view, name="impact_view"),
+    path("initiatives/", views.initiatives_view, name="initiatives_view"),
+    path("innovation/", views.innovation_view, name="innovation_view")
 ]
--- DigitalDevelopment/main/views.py
+++ DigitalDevelopment/main/views.py
@@ -2,6 +2,33 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+#Home page
 def home_view(request : HttpRequest):
 
     return render( request, "main/home.html")
+
+#Future page
+def future_view(request : HttpRequest):
+
+    return render( request, "main/future.html")
+
+#المبادرات
+def initiatives_view(request : HttpRequest):
+
+    return render( request, "main/initiatives.html")
+
+#Creative page
+def innovation_view(request : HttpRequest):
+
+    return render( request, "main/innovation.html")
+
+#Impact page
+def impact_view(request : HttpRequest):
+
+    return render( request, "main/impact.html")
+
+#Fields page
+def fields_view(request : HttpRequest):
+
+    return render( request, "main/fields.html")
