860d33b6761bd0797225283f07dcf5dcbf7a27f8
abdulmajeed <mjeed.f.b@outlook.sa>
2025-11-14 10:36:14 +0300

    create navbar

--- OurKingdom/SaudiaArabia/views.py
+++ OurKingdom/SaudiaArabia/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 
-def home_page(request):
-    return HttpResponse("home page")
+def home_page(request:HttpRequest):
+    return render(request, "SaudiaArabia/home.html")
 
-def history_page(request):
-    return HttpResponse("history page")
+def history_page(request:HttpRequest):
+    return render(request, "SaudiaArabia/history.html")
 
-def tourism_page(request):
-    return HttpResponse("tourism page")
+def tourism_page(request:HttpRequest):
+    return render(request, "SaudiaArabia/tourism.html")
 
-def culture_page(request):
-    return HttpResponse("culture page")
+def culture_page(request:HttpRequest):
+    return render(request, "SaudiaArabia/culture.html")
 
-def manufacturing_page(request):
-    return HttpResponse("manufacturing page")
+def manufacturing_page(request:HttpRequest):
+    return render(request, "SaudiaArabia/manufacturing.html")
 
-def vision_page(request):
-    return HttpResponse("vision page")
+def vision_page(request:HttpRequest):
+    return render(request, "SaudiaArabia/vision.html")
 
-def education_page(request):
-    return HttpResponse("education page")
+def education_page(request:HttpRequest):
+    return render(request, "SaudiaArabia/education.html")
